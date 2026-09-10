cc14a0277f499c6c23a1e0e23392f1a87fa44698
CassieBonnettYT1 <notbadyt809@gmail.com>
2026-09-10 17:25:20 -0400

    Update uv.config.js

--- uv.config.js
+++ uv.config.js
@@ -1,6 +1,6 @@
 self.__uv$config = {
     prefix: '/studdy-app/service/',
-    bare: 'https://bare.dev', // Handles the unblocking traffic network
+    bare: 'https://tomp.app',
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
     handler: 'https://jsdelivr.net',
