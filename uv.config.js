f3c87d3fd36ff16984468d0e9b36549fd5d20887
CassieBonnettYT1 <notbadyt809@gmail.com>
2026-09-10 17:38:11 -0400

    Update UV configuration paths and handlers

--- uv.config.js
+++ uv.config.js
@@ -1,10 +1,10 @@
 self.__uv$config = {
-    prefix: '/studdy-app/service/',
-    bare: 'https://studentcenter.biz',
+    prefix: '/service/',
+    bare: '/bare/',
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
-    handler: 'https://jsdelivr.net',
-    bundle: 'https://jsdelivr.net',
-    config: '/studdy-app/uv.config.js',
-    sw: '/studdy-app/uv.sw.js',
+    handler: '/uv/uv.handler.js',
+    bundle: '/uv/uv.bundle.js',
+    config: '/uv/uv.config.js',
+    sw: '/uv/uv.sw.js',
 };
